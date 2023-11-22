e3bf454f439947d065f27518ce708293936fdd40
WillowWay <140159547+WillowWay@users.noreply.github.com>
2023-11-22 15:57:50 +0800

    Add files via upload

--- webqr.js
+++ webqr.js
@@ -114,7 +114,7 @@ function read(a)
     
     if(a=="http://192.168.1.2:10200/NISE3900")
     {
-        el.innerHTML = '<li><a href="http://192.168.1.2:10200/main" target="_blank"> Home </a></li><li><a href="http://192.168.1.2:10200/NISE3900_1" target="_blank"> 履歷明細 </a></li><li><a href="http://192.168.1.2:10200/NISE3900_TS" target="_blank"> 交大系統 </a></li><li><a href="http://192.168.1.2:10200/NISE3900_V1" target="_blank"> 鎖板VID </a></li><li><a href="http://192.168.1.2:10200/NISE3900_V2" target="_blank"> 外殼VID </a></li><li><a href="http://192.168.1.2:10200/NISE3900_V3" target="_blank"> 包裝VID </a></li>';
+        el.innerHTML = '<li><a href="http://192.168.1.2:10200/main" onclick="openLink(event)"> Home </a></li><li><a href="http://192.168.1.2:10200/NISE3900_1" onclick="openLink(event)"> 履歷明細 </a></li><li><a href="http://192.168.1.2:10200/NISE3900_TS" onclick="openLink(event)"> 交大系統 </a></li><li><a href="http://192.168.1.2:10200/NISE3900_V1" onclick="openLink(event)"> 鎖板VID </a></li><li><a href="http://192.168.1.2:10200/NISE3900_V2" onclick="openLink(event)"> 外殼VID </a></li><li><a href="http://192.168.1.2:10200/NISE3900_V3" onclick="openLink(event)"> 包裝VID </a></li>';
         dt.innerHTML ='<p> Product Name: NISE3900</p><p>Product S/N: M30001CVZDVCG6B</p><p> Product Emissions: 140.28 KgCO2e</p><p> Product Started Time: 2023/1/7 15:51:10</p><p> Product Ended Time: 2023/1/19 06:52:22</p>'
         document.getElementById("myLink").addEventListener("click", function(event) {
             event.preventDefault(); // 阻止默认的链接跳转行为
